069b79b4f2f1f6e91ad2d8a6b5fe640dc849c11b
AdamLaptop <fireworksanimations21@gmail.com>
2026-07-28 20:19:53 +0900

    Begin gauge
    Knoppen tellen nu op tot 5

--- Assets/_game/Scripts/UI/OnGameButtonPressed.cs
+++ Assets/_game/Scripts/UI/OnGameButtonPressed.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,25 +9,82 @@ using UnityEngine.UIElements;
 public class OnGameButtonPressed : MonoBehaviour
 {
     [SerializeField] private UIDocument _uiDocument;
+    [SerializeField] private float maxTime = 5f;
+
+    private Button btnTop, btnBottom;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         var root = _uiDocument.rootVisualElement;
 
-        var btnTop = root.Q<Button>("TopButton");
-        var btnBottom = root.Q<Button>("BottomButton");
+        btnTop = root.Q<Button>("TopButton");
+        btnBottom = root.Q<Button>("BottomButton");
 
-        btnTop.clicked += OnTopButtonPressed;
-        btnBottom.clicked += OnBottomButtonPressed;
+        btnTop.RegisterCallback<PointerDownEvent>(OnButtonPressed, TrickleDown.TrickleDown);
+        btnTop.RegisterCallback<PointerUpEvent>(OnButtonReleased);
+
+        btnBottom.RegisterCallback<PointerDownEvent>(OnButtonPressed, TrickleDown.TrickleDown);
+        btnBottom.RegisterCallback<PointerUpEvent>(OnButtonReleased);
     }
 
-    void OnTopButtonPressed()
+    void OnDisable()
     {
-        Debug.Log("Testing the top button");
+        btnTop.UnregisterCallback<PointerDownEvent>(OnButtonPressed, TrickleDown.TrickleDown);
+        btnTop.UnregisterCallback<PointerUpEvent>(OnButtonReleased);
+
+        btnBottom.UnregisterCallback<PointerDownEvent>(OnButtonPressed, TrickleDown.TrickleDown);
+        btnBottom.UnregisterCallback<PointerUpEvent>(OnButtonReleased);
     }
 
-    void OnBottomButtonPressed()
+    IEnumerator TopButtonCoroutine;
+    IEnumerator BottomButtonCoroutine;
+    void OnButtonPressed(PointerDownEvent e)
     {
-        Debug.Log("Testing the Bottom button");
+        var btn = e.target as Button;
+
+        var routine = WaitForSeconds(maxTime, btn);
+        if (btn == btnTop)
+            TopButtonCoroutine = routine;
+        else
+            BottomButtonCoroutine = routine;
+
+        StartCoroutine(routine);
     }
+
+    void OnButtonReleased(PointerUpEvent e)
+    {
+        var btn = e.target as Button;
+        IEnumerator routine = null;
+        if (btn == btnTop)
+        {
+            routine = TopButtonCoroutine;
+            TopButtonCoroutine = null;
+        }
+        else
+        {
+            routine = BottomButtonCoroutine;
+            BottomButtonCoroutine = null;
+        }
+
+        StopCoroutine(routine);
+    }
+
+
+    IEnumerator WaitForSeconds(float seconds, Button btn)
+    {
+        float elapsedTime = 0f;
+        Debug.Log("button name: " + btn.name);
+
+        while (elapsedTime < seconds)
+        {
+            elapsedTime += Time.deltaTime;
+            // Debug.Log("clamped time: " + Mathf.FloorToInt(elapsedTime) + " seconds");
+            EventManager.Instance.TriggerDelegate("GaugeTime", Mathf.FloorToInt(elapsedTime), btn); //update the guages.
+            yield return null;
+        }
+    }
+
+
 }
